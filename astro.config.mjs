05f5f851d2ef68830445bec0d4784cd172bd71b7
ets-project1-2025 <ets.project1.2025@gmail.com>
2025-10-02 14:29:50 +0000

    Perbaikan error 404 setelah deployment vercel berhasil

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server', // Menggunakan server-side rendering
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel({ 
+    mode: 'serverless' // Menggunakan mode serverless untuk Vercel
+  }),
   server: {
     port: 4321,
     host: true  // Mengizinkan akses dari alamat IP eksternal (penting untuk codespaces)
--- dist/server/chunks/config_CansV41r.mjs
+++
@@ -1,3 +0,0 @@
-const SITE_URL = "https://furniturekami.vercel.app";
-
-export { SITE_URL as S };
